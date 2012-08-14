1efccded8b20439dc68a59d77eae188d7c0ffa60
Junya Ogino <ogijun@gmail.com>
2012-08-14 18:35:48 +0900

    Hide ads.

---
+++ tabelog.com.js
@@ -0,0 +1,9 @@
+(function(){
+    $('#rstdx-header .catchcopy').hide();
+    $('.listing').hide();
+    $('.banner').hide();
+    $('#premium_coupon_derivation_rstdtl_main').hide();
+    $('#coupon_restaurants').hide();
+    $('.areamatch-box').hide();
+    $('#areamatch-box').hide();
+}())
